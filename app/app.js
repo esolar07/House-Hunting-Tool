3e9e4c5cedef74cb2de9dff73bb8e0f7b0ebcfee
esolar07 <esolar07@gmail.com>
2017-03-15 13:41:30 -0400

    add gsap functionality to light box. Added display and remove btn. made styles changes

--- app/app.js
+++ app/app.js
@@ -8,17 +8,30 @@ var address = document.querySelector("#address"),
     price = document.querySelector("#price"),
     add = document.querySelector('#display-lightbox'),
     lightbox = document.querySelector(".l-section-lightbox"),
-    updateButton = document.querySelector("#updateButton");
+    form = document.querySelector('form'),
+    fields = document.querySelectorAll("fieldset"),
+    updateButton = document.querySelector("#updateButton"),
+    submit = document.querySelector("#submit-btn"),
+    remove = document.querySelector("#remove-lightbox"),
+    tl = new TimelineMax();
 
 console.log("loaded");
 // shows add listing form
 var displayLightbox = () =>{
     add.addEventListener('click', function(){
-        console.log("clicked");
-        lightbox.style.transform = "scale(1)";
-        lightbox.style.borderRadius = 0;
+        tl.to(lightbox, .1, {css:{transform: "scale(1)"}, ease:Power4.easeOut})
+        .from(form, .5,{y:-50, autoAlpha:0, ease:Sine.easeOut},'+=0.1')
+        .staggerFrom(fields, .5, {y:-10, autoAlpha:0, ease:Sine.easeOut},'+=0.1')
+        .from(submit, .5,{y:-30, autoAlpha:0, ease:Sine.easeOut},'+=0.1')
     });
 }
 
+var removeLightbox = () => {
+    remove.addEventListener('click', function(){
+        console.log("clicked remove");
+        tl.to(lightbox, .9, {css:{transform: "scale(0)"}, ease:Power4.easeOut})
+    });
+}
 
 displayLightbox();
+removeLightbox();
